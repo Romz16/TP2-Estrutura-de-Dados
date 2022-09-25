359d4b690fcf8833a360e367b371f2d230b3b3ff
Romz <romulo160802@gmail.com>
2022-09-25 11:51:14 -0300

    add blocos 2f

--- src/headers/intercalacao2F.h
+++ src/headers/intercalacao2F.h
@@ -5,9 +5,11 @@
 #include <stdio.h>
 #include <time.h>
 #include "structs.h"
+#include "sortInterno.h"
+
 
 void ordenar_blocos(FILE*,FILE**,int);
-void Insertion(TipoBloco*,int);
+
 void intercalar_blocos(FILE**,int);
 
 
